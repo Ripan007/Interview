211e96236e94e664c6cd585d7dc34ed0534ab186
ripan <reponmondal007@gmail.com>
2023-12-08 10:22:20 +0000

    Dec 8, 2023, 3:52 PM

--- JS/NamasteJavascript/script.js
+++ JS/NamasteJavascript/script.js
@@ -1,7 +1,7 @@
 /**
 * * How JavaScript Works 🔥& Execution Context:
 - everything in javascript happen inside an Execution Context(assume it a big box / container in which the whole javascript  code gets executed)
-- Execution context : two component => memory component :
+- Execution context : two component => memory component / variable environment :
 - memory => store variable/functions => key value pair
 
 
