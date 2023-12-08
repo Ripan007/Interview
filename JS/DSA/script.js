25c2d507086d65b8a62b752dc06274669ab285cd
ripan <reponmondal007@gmail.com>
2023-12-08 14:07:47 +0000

    Dec 8, 2023, 7:37 PM

--- JS/DSA/script.js
+++ JS/DSA/script.js
@@ -1,5 +1,6 @@
 /*
 * * Big O' => how well the problem is solved
+* * what is good code ? readable , scalable (Big O)
 1	    Constant	statement, one line of code
 log(n)	Logarithmic	Divide and conquer (binary search)
 n	    Linear	Loop
@@ -7,3 +8,16 @@ n*log(n)	Linearithmic	Effective sorting algorithms
 n^2	    Quadratic	Double loop
 n^3	    Cubic	Triple loop
 2^n	    Exponential	Complex full search */
+
+const nemo = ['nemo']
+function findNemo(array) {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === 'nemfo') {
+      console.log('nemo found')
+    } else {
+      console.log('nemo not found')
+    }
+  }
+}
+
+findNemo(nemo)
