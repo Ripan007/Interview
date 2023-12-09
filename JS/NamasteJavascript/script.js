9af150148a1863920a7916dfc483c4c0df27c60f
ripan <reponmondal007@gmail.com>
2023-12-09 10:09:39 +0000

    Dec 9, 2023, 3:39 PM

--- JS/NamasteJavascript/script.js
+++ JS/NamasteJavascript/script.js
@@ -136,14 +136,20 @@ function y() {
 }
 y()
 
+function z() {
+  for (let i = 1; i <= 5; i++) {
+    function close(i) {
+      setTimeout(() => {
+        console.log(i)
+      }, i * 1000)
+    }
+    close(i)
+  }
+}
+z()
+
 
 
 
 
 */
-
-function z() {
-  for (let i = 1; i <= 5; i++) {
-    function close() {}
-  }
-}
