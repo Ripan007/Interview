d7b8ade6a563dd4b2ddcabb48c3108eeac1ad9d8
ripan <reponmondal007@gmail.com>
2023-12-10 03:25:24 +0000

    Dec 10, 2023, 8:55 AM

--- JS/DSA/script.js
+++ JS/DSA/script.js
@@ -133,6 +133,13 @@ let is blocked scope
 
 */
 
-const Player(){
+const Player ={
+  constructor(name,type){
+    this.name = name;
+    this.type = type;
+}
+  introduce(){
+    console.log();
 
+  }
 }
