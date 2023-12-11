a6fbbcb65aab15c66ec98c56df67d7d4ea37dd95
ripan <reponmondal007@gmail.com>
2023-12-11 03:02:21 +0000

    Dec 11, 2023, 8:32 AM

--- JS/Interview.js
+++ JS/Interview.js
@@ -1,4 +1,7 @@
 /*
 * * what  is  the difference between java and javascript ?
--
+- Java is an object-based, strongly typed language.
+- It’s used in app development and runs on a virtual machine.
+- It’s also known for block scoping and thread-based concurrency, which consumes more memory
+
 */
