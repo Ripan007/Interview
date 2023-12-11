9ac130e56dd50bbf803ca7e2727b0f1299a0a234
ripan <reponmondal007@gmail.com>
2023-12-11 03:04:21 +0000

    Dec 11, 2023, 8:34 AM

--- JS/Interview.js
+++ JS/Interview.js
@@ -1,5 +1,9 @@
 /*
-* * what  is  the difference between java and javascript ?
+1. what  is  the difference between java and javascript ?
+
+* Java is an object-based, strongly typed language. It’s used in app development and runs on a virtual machine. It’s also known for block scoping and thread-based concurrency, which consumes more memory
 
 * JavaScript is an object-oriented, dynamically typed language. JavaScript is used for web applications in browsers and Node.js environments. It utilizes function-scoping event-based concurrency and is more memory-efficient. This makes it ideal for web pages
+
+2. What are the seven JavaScript data types?
 */
