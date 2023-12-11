647956aec474dfd23b400b1532c587fc6b2d26e6
ripan <reponmondal007@gmail.com>
2023-12-11 03:05:50 +0000

    Dec 11, 2023, 8:35 AM

--- JS/Interview.js
+++ JS/Interview.js
@@ -6,4 +6,8 @@
 * JavaScript is an object-oriented, dynamically typed language. JavaScript is used for web applications in browsers and Node.js environments. It utilizes function-scoping event-based concurrency and is more memory-efficient. This makes it ideal for web pages
 
 2. What are the seven JavaScript data types?
+* string ,number ,bigint , boolean , undefined  , null, symbol, object
+
+3. What is JSON in JavaScript?
+
 */
